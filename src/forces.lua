f993c0489a909f626a4d71715352ade5e60cddac
Adam Heinermann <aheinerm@gmail.com>
2022-08-20 04:05:06 -0700

    Set force colours

--- src/forces.lua
+++ src/forces.lua
@@ -1,19 +1,18 @@
 local forces = {}
 
--- TODO: Use new force.custom_color write
-
-local function create_new_force(forcename)
+local function create_new_force(forcename, color)
     local newforce = game.create_force(forcename)
 
     newforce.disable_research()
     newforce.disable_all_prototypes()
     newforce.ai_controllable = false
+    newforce.custom_color = color
 end
 
 function forces.on_init()
-    create_new_force("terran")
-    create_new_force("protoss")
-    create_new_force("zerg")
+    create_new_force("terran", {12, 72, 204})
+    create_new_force("protoss", {252, 252, 56})
+    create_new_force("zerg", {136, 64, 156})
 end
 
 return forces
